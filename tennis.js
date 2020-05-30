d10a05341241748dd64617892d1593d31b1cdbe2
azagorowski <37112877+azagorowski@users.noreply.github.com>
2020-05-30 11:53:32 +0200

    bugfix scrolling speed

--- tennis.js
+++ tennis.js
@@ -187,3 +187,33 @@ function colorRect(leftX, topY, width, height, drawColor) {
 	canvasContext.fillStyle = drawColor;
 	canvasContext.fillRect(leftX, topY, width, height);
 }
+
+/* Scrolling speed
+
+Source:
+
+How to change default scrollspeed,scrollamount,scrollinertia of a webpage
+https://stackoverflow.com/questions/14426548/how-to-change-default-scrollspeed-scrollamount-scrollinertia-of-a-webpage#answer-14430585
+*/
+
+ function wheel(event) {
+      var delta = 0;
+      if (event.wheelDelta) {(delta = event.wheelDelta / 120);}
+      else if (event.detail) {(delta = -event.detail / 3);}
+
+      handle(delta);
+      if (event.preventDefault) {(event.preventDefault());}
+      event.returnValue = false;
+  }
+
+  function handle(delta) {
+      var time = 1000;
+      var distance = 300;
+
+      $('html, body').stop().animate({
+          scrollTop: $(window).scrollTop() - (distance * delta)
+      }, time );
+  }
+
+  if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
+    window.onmousewheel = document.onmousewheel = wheel;
